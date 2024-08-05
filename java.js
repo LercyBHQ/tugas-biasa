57ed88ba2d20cd47ac1783cca1023b3b71a3900a
LercyBHQ <baihaqifijar8686Gmail.com>
2024-08-05 10:12:11 +0700

    first commit

--- java.js
+++ java.js
@@ -1,10 +1,18 @@
-function validasi()
+function validasi1()
 {
     var cek = document.forms["amb"]["nama"].value;
     if (!/^[a-zA-Z]+$/.test(cek))
     {
         alert("Silakan isi form terlebih dahulu dengan huruf");
         document.getElementById("nama").focus();
+        document.amb.nama.value="";
+        return false;
+    }
+    var cek = document.forms["amb"]["buku"].value;
+    if (!/^[a-zA-Z]+$/.test(cek))
+    {
+        alert("Silakan isi form terlebih dahulu dengan huruf");
+        document.getElementById("buku").focus();
         document.amb.nama.value="";
         return false;
     }
